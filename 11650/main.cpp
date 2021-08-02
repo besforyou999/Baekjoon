d5c20f4e4f93739812262d55d4c25e4f43cae54c
Sangmin <besforyou2@gmail.com>
2021-08-02 11:48:54 +0900

    sorting

---
+++ 11650/main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class coord {
+	public :
+		int x;
+		int y;
+
+	coord(int x, int y) {
+		this->x = x;
+		this->y = y;		
+	}
+};
+
+bool compare(coord & a, coord & b) {
+
+	if (a.x < b.x ) {
+		return true;
+	} 
+	else if ( a.x == b.x) {
+		if (a.y < b.y) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	else 
+		return false;
+}
+
+
+int main (void) {
+
+	int n;
+	cin >> n;
+	int x , y = 0;
+
+	vector<coord> v;
+
+	for (int i = 0; i < n ; i++) {
+		cin >> x >> y;
+		v.push_back(coord(x,y));
+	}
+
+	sort(v.begin(), v.end() , compare);
+
+	for (int i = 0; i < n ; i++ ) {
+		cout << v[i].x << " " << v[i].y << endl;
+	}
+
+	return 0;
+}
+
+
+
