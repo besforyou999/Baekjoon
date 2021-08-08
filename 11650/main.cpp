b8c0c8ad0e0b361d4b0976dacff6acb98021b569
Sangmin <besforyou2@gmail.com>
2021-08-08 12:43:31 +0900

    vector sort algorithm

--- 11650/main.cpp
+++ 11650/main.cpp
@@ -4,58 +4,45 @@
 
 using namespace std;
 
-class coord {
+class COD {
 	public :
 		int x;
 		int y;
 
-	coord(int x, int y) {
-		this->x = x;
-		this->y = y;		
+	COD(int _x, int _y) {
+		x = _x;
+		y = _y;
 	}
 };
 
-bool compare(coord & a, coord & b) {
-
-	if (a.x < b.x ) {
+bool compare (COD & a, COD & b) {
+	if ( a.x < b.x ) {
 		return true;
-	} 
-	else if ( a.x == b.x) {
-		if (a.y < b.y) {
-			return true;
-		}
-		else {
-			return false;
-		}
 	}
-	else 
+	else if ( a.x == b.x) {
+		if ( a.y < b.y ) 
+			return true;
+		else
+			return false;
+	}
+	else
 		return false;
 }
 
-
 int main (void) {
-
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	int n;
 	cin >> n;
-	int x , y = 0;
-
-	vector<coord> v;
-
-	for (int i = 0; i < n ; i++) {
-		cin >> x >> y;
-		v.push_back(coord(x,y));
-	}
-
-	sort(v.begin(), v.end() , compare);
-
+	int x = 0, y = 0;
+	vector<COD> v;
 	for (int i = 0; i < n ; i++ ) {
-		cout << v[i].x << " " << v[i].y << endl;
+		cin >> x >> y;
+		v.push_back(COD(x,y));
 	}
-
-	return 0;
+	sort(v.begin(), v.end() , compare);
+	for (int i = 0; i < v.size() ; i++ ) 
+		cout << v[i].x << " " << v[i].y <<  endl;
 }
-
 
 
