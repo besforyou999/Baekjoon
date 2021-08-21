069a03d8982dcf9c066fbb03533a262f5ea8637f
Sangmin <besforyou2@gmail.com>
2021-08-21 15:11:03 +0900

    dp

---
+++ 9655/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int arr[1001];
+
+int main (void) {
+
+	int n;
+	cin >> n;
+
+	if ( n % 2 == 0 ) {
+		cout << "CY";
+	}
+	else
+		cout << "SK";
+
+
+return 0;
+}
+	
