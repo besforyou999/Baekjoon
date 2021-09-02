94c57d5f380112ac668785e06ff718078d6a0fbf
Sangmin <besforyou2@gmail.com>
2021-09-02 19:11:07 +0900

    dp

--- freeDir/main.cpp
+++ freeDir/main.cpp
@@ -2,29 +2,28 @@
 
 using namespace std;
 
+int n;
+int arr[1001];
 int dp[1001];
-int parr[1001];
-int N;
 
-int Bigger(int A, int B) { if (A>B) return A; return B;}
+int main(void) {
 
-int main (void) {
+	cin >> n;
 
-	cin >> N;
+	for (int i = 1 ; i <= n ; i++ ) 
+		cin >> arr[i];
 
-	for (int i = 1; i<= N ; i++) cin >> parr[i];
+	int max = -1;
 
-	for (int i = 1; i <= N ; i++ ) {
-		for (int j = 1 ; j <= i ; j++ ) {
-			dp[i] = Bigger(dp[i], dp[i-j] + parr[j]);
+	for ( int i = 1; i <= n ; i++ ) {
+		dp[i] = 1;
+		for ( int j = 1 ; j <= i ; j++ ) {
+			if ( arr[i] <arr[j] && dp[i] < dp[j] + 1) 
+				dp[i] = dp[j] + 1;
 		}
+		if ( max < dp[i] ) max = dp[i];
 	}
 
-	cout << dp[N] << endl;
-	
+	cout << max;	
 	return 0;
-}	
-		
-
-			
-	
+}
