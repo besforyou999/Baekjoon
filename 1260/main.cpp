008ce8756243423280db57fc04fd327362038088
Sangmin <besforyou2@gmail.com>
2021-10-02 20:34:48 +0900

    graph DFS, BFS

--- 1260/main.cpp
+++ 1260/main.cpp
@@ -1,38 +1,68 @@
 #include <iostream>
+#include <list>
 #include <queue>
 
 using namespace std;
 
 const int MAX = 1001;
-
+int N, M, V;
+int visited[MAX];
 int mat[MAX][MAX];
-int visit[MAX];
-int N, M , V;
+queue<int> q;
 
-int main(void) {
+void BFS( int s ) {
 
+	q.push(s);
+	visited[s] = 1;
+
+	while (!q.empty()) {
+		s = q.front();
+		cout << s << " ";
+		q.pop();
+		for ( int i = 1 ; i <= N ; ++i) {
+			if ( mat[s][i] == 1 && visited[i] == 0 ) {
+				q.push(i);
+				visited[i] = 1;
+			}
+		}
+	}
+}
+
+void DFS(int v) {
+	visited[v] = 1;
+	cout << v << " ";
+	for ( int i = 1 ; i <= N ; ++i ) {
+		if ( mat[v][i] == 1 && visited[i] == 0 )	
+			DFS(i);
+	}
+}
+
+void reset() {
+	for ( int i = 1; i <= N ; i++ ) {
+		visited[i] = 0;
+	}
+	cout << endl;
+}
+
+int main (void) {
+	
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int first, second;
 	cin >> N >> M >> V;
 
-	for ( int i = 0; i < M ; i++ ) {
-		int first, second;
+	for ( int i = 0 ; i < M ; i++ ) {
 		cin >> first >> second;
-		mat[first][second] = mat[second][first] = 1;
+		mat[first][second] = 1;
+		mat[second][first] = 1;
 	}
 
+	DFS(V);
+	reset();
+	BFS(V);
+
+	return 0;
 }
 
-void dfs (int v) {
-	cout << v << " ";
-	visit[v] = 1;
-	for ( int i = 1; i <= N ; i++ ) {
-		if ( visit[v] == 1 || mat[v][i] == 0 )
-			continue;
-		dfs(i);
-	}
-}	
-
-void bfs (int v) {
-	queue<int> q;
-	visit[v] = 0;
-
-}
