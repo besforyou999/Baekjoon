71d85b8b14ae55891c6c8562ec8aa665c9fa4fec
Sangmin <besforyou2@gmail.com>
2021-10-07 23:32:48 +0900

    maze problem

--- 2178/Main.java
+++ 2178/Main.java
@@ -1,85 +1,63 @@
 import java.util.Scanner;
-import java.util.queue;
+import java.util.Queue;
 import java.util.LinkedList;
 
 public class Main {
 
-	static final int MAX = 101;
-	static int N, M;
-	static int mat[][] = new int[MAX][MAX];
-	static int path[][] = new int[MAX][MAX];
-	static boolean visited[][] = new boolean[MAX][MAX];
-	
-	public static void BFS (int y, int x) {
-		
-		path[y][x] = 1;
-		visited[y][x] = true;
+	static int [][] mat;
+	static int n , m;
+	static boolean [][] visited;
+	static int[] dx = { -1, 1, 0, 0 };
+	static int[] dy = { 0, 0, -1, 1 };
 
-		queue.add(new Pair(y,x));
-
-		while ( !queue.empty() ) {
-
-			Pair temp = queue.poll();
-			y = temp.y;
-			x = temp.x;
-
-			for ( int i = 0 ; i < 4 ; i++ ) {
-
-				int ny = y + dy[i];
-				int nx = x + dx[i];
-
-				if ( ny < 0 || nx < 0 || ny >= N || nx >= M	) 
-					continue;
-				if ( mat[ny][nx] == 1 && visited[ny][nx] == false ) {
-					visited[ny][nx] = true;
-					queue.add(new Pair(ny,nx));
-					path[ny][nx] = path[y][x] + 1;
-				}
-			}
-		}
-	}
-
-	public static void main ( String []args ) {
+	public static void main (String [] args) {
 
 		Scanner sc = new Scanner(System.in);
 
-		N = sc.nextInt();
-		M = sc.nextInt();
+		n = sc.nextInt();
+		m = sc.nextInt();
 
-		String str = new String();
+		mat = new int[n][m];
 
-		for ( int i = 0 ; i < N ; i++ ) {
-			str = sc.next();
-			for ( int j = 0; j < M ; j++ ) 
-				mat[i][j] = str.charAt(j) - '0';
+		String inputStr = new String();
+
+		for ( int i = 0 ; i < n ; i++ ) {
+			inputStr = sc.next();
+			for ( int j = 0 ; j < m ; j++ ) {
+				mat[i][j] = inputStr.charAt(j) - '0';
+			}
 		}
 
-		int [] dx = { 1, 0 , -1 , 0 };
-		int [] dy = { 0 , 1 , 0 , -1 };
+		visited = new boolean[n][m];
+		visited[0][0] = true;
+		bfs(0, 0);
+		System.out.println(mat[n-1][m-1]);
+	}
 
-		Queue<Pair> queue = new LinkedList<>();
+	public static void bfs(int x,int y) {
 
-		queue.add(new Pair(1, 1));
-		visited[1][1] = true;
+		Queue<int[]> queue = new LinkedList<>();
 
+		queue.add(new int[] {x,y});
 
-		
+		while ( !queue.isEmpty() ) {
+			int temp[] = queue.poll();
+			int newX = temp[0];
+			int newY = temp[1];
 
+			for ( int i = 0; i < 4 ; i++ ) {
+				int nextX = newX + dx[i];
+				int nextY = newY + dy[i];
 
+				if ( nextX < 0 || nextX >= n || nextY < 0 || nextY >= m )
+					continue;
+				if ( visited[nextX][nextY] || mat[nextX][nextY] == 0 )
+					continue;
 
-		System.out.print(mat[N-1][M-1]);
-
-		return ;
+				queue.add(new int[] {nextX, nextY});
+				mat[nextX][nextY] = mat[newX][newY] + 1;
+				visited[nextX][nextY] = true;
+			}
+		}
 	}
 }
-
-public class Pair {
-
-	int x, y;
-	Pair(int x, int y) {
-		this.x = x;
-		this.y = y;
-	}
-}
-
-
