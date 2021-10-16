e38ac77d58e6205ae97dae49198d862782286829
Sangmin <besforyou2@gmail.com>
2021-10-16 23:07:15 +0900

    fib

--- fib/fib.cpp
+++ fib/fib.cpp
@@ -2,11 +2,22 @@
 
 using namespace std;
 
+int Fib[1001];
+
 int fib(int n) {
 	if ( n <= 1 ) return n;
 
 	return fib(n-2) + fib(n-1);
 
+}
+
+void fib2(int n) {
+	Fib[0] = 0;
+	Fib[1] = 1;
+
+	for ( int i = 2 ; i <= n ; i++ ) {
+		Fib[i] = Fib[i-1] + Fib[i-2];
+	}
 }
 
 int main(void) {
