a6aa044b1d6afb94d1f3d5b33ff6bd0cf0b7cbe4
Sangmin <besforyou2@gmail.com>
2021-11-07 23:06:08 +0900

    test

---
+++ 4963/test.java
@@ -0,0 +1,12 @@
+public class test {
+
+	public static void main(String [] args) {
+
+		int [][] mat = { {1,2,3 }, {4,5,6}, {7,8,9}};
+
+		for ( int i = 0; i < 3 ; i++ ) {
+			System.out.print(mat[0][i] + " ");
+		}
+
+	}
+}
--- Main.java
+++ Main.java
@@ -1,12 +1,19 @@
 import java.util.Scanner;
 
 public class Main {
+	static String [][] mat;
 	public static void main(String []args) {
+		
+		String [] names = { "Pizza arraak 145", "Bread kkk 30" };
 
-		Scanner sc = new Scanner(System.in);
+		String []temp1 = names[0].split(" ");	
 
+		for ( int i = 0 ; i < temp1.length ; i++ ) {
+			System.out.println(temp1[i]);
+		}
+		Character test = 'a';
+		String test2 = test.toString();
 
-
-		return ;
+		System.out.println(test2);
 	}
 }
