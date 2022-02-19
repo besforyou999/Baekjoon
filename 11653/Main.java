d6d0ae76fe29a359ca2b2a8186e6769092ab434a
Sangmin <besforyou2@gmail.com>
2022-02-19 14:54:01 +0900

    BAEK 11653 - prime factorization

---
+++ 11653/Main.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.ArrayList;
+
+public class Main {
+	public static void main(String []args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int N = Integer.parseInt(br.readLine());
+	
+		for (int i = 2 ; i <= Math.sqrt(N) ; i++) {
+			while(N % i == 0) {
+				bw.write(i + "\n");
+				N /= i;
+			}
+		}
+
+		if (N != 1) bw.write(N + "\n");
+
+		bw.flush();
+		bw.close();
+	}
+}
