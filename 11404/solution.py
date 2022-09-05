c3cc220395da62e2cff44024e5d0efbc16083b4c
Sangmin <besforyou2@gmail.com>
2022-09-05 18:34:10 +0900

    11404. 플로이드 (Python)

---
+++ 11404/solution.py
@@ -0,0 +1,45 @@
+import sys
+input = sys.stdin.readline
+
+MAX = 1e9
+
+
+def Init():
+    for row in range(n):
+        for col in range(n):
+            if row != col:
+                graph[row][col] = MAX
+
+
+def Floyd():
+    for mid in range(0, n):
+        for s in range(0, n):
+            for e in range(0, n):
+                if s == e:
+                    graph[s][e] = 0
+                elif graph[s][e] > graph[s][mid] + graph[mid][e]:
+                    graph[s][e] = graph[s][mid] + graph[mid][e]
+
+
+n = int(input())
+m = int(input())
+graph = [[MAX for _ in range(n)] for _ in range(n)]
+
+Init()
+for i in range(m):
+    x, y, c = map(int, input().split())
+    graph[x-1][y-1] = min(c, graph[x-1][y-1])
+
+Floyd()
+
+answer = ""
+
+for i in range(n):
+    for j in range(n):
+        if graph[i][j] == MAX:
+            print(0, end=' ')
+        else:
+            print(graph[i][j], end=' ')
+    print()
+
+
