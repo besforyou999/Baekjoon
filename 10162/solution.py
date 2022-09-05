404bce609670ecf569b827d0a4b3a43557e604a7
Sangmin <besforyou2@gmail.com>
2022-09-05 14:19:09 +0900

    10162. 전자레인지 (Python)

---
+++ 10162/solution.py
@@ -0,0 +1,38 @@
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+
+answer = ""
+A = 300
+countA = 0
+B = 60
+countB = 0
+C = 10
+countC = 0
+
+if T >= A:
+    divA = int(T / A)
+    remain = int(T % A)
+    countA = divA
+    T = remain
+
+if T >= B:
+    divB = int(T / B)
+    remain = int(T % B)
+    countB = divB
+    T = remain
+
+if T >= C:
+    divC = int(T / C)
+    remain = int(T % C)
+    countC += divC
+    T = remain
+
+if remain != 0:
+    answer = -1
+elif remain == 0:
+    answer = str(countA) + " " + str(countB) + " " + str(countC)
+
+print(answer)
+
