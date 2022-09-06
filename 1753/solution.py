9d1f2eed7678cf73787885874f7c3b7faa3e05fe
Sangmin <besforyou2@gmail.com>
2022-09-06 16:24:28 +0900

    1753. 최단경로 (Python)

---
+++ 1753/solution.py
@@ -0,0 +1,43 @@
+import heapq
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+INF = sys.maxsize
+V, E = map(int, input().split())
+K = int(input())
+
+# 가중치 dp
+distance = [INF] * (V + 1)
+graph = [[] * (V + 1) for _ in range(V + 1)]
+
+for _ in range(E):
+    a, b, c = map(int, input().split())
+    graph[a].append((b, c))
+
+
+def dijkstra(start):
+    q = []
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+
+    while q:
+        dist, now = heapq.heappop(q)
+        if distance[now] < dist:
+            continue
+        for i in graph[now]:
+            cost = dist + i[1]
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+
+
+dijkstra(K)
+
+
+for i in range(1, V + 1):
+    if distance[i] == INF:
+        print("INF")
+    else:
+        print(distance[i])
+
+
