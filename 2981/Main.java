def738dc36e0317d64cb377efd900a046b84c6d0
Sangmin Ju <besforyou2@gmail.com>
2023-01-26 17:58:45 +0900

    2981. 검문 (Java)

--- 2981/Main.java
+++ 2981/Main.java
@@ -1,39 +1,42 @@
-import java.util.Scanner;
-import java.util.Arrays;
+package codingProblems;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String [] args) {
-		Scanner sc = new Scanner(System.in);
-
-		int N = sc.nextInt();
-
+	public static void main(String [] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
 		int [] arr = new int[N];
-
-		for (int i = 0; i < N ; i++ ) 
-			arr[i] = sc.nextInt();
-	
-		Arrays.sort(arr);
-
-		int smallest = arr[0];
-
-		ArrayList<Integer> array_of_M = new ArrayList<Integer>();
-
-		for (int i = 2 ; i < smallest ; i++ ) {
-			int same = 1;
-			int divResult = arr[0] % i;
-			for (int j = 1 ; j < N ; j++ ) {
-				 if ( divResult != arr[j] % i ) {
-					 same = 0;
-					 break;
-				 }
-			}
-
-			if ( same == 1 ) 
-				array_of_M.add(i);			
+		
+		for (int i = 0 ; i < N ; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
 		}
-
-		for (int i = 0; i < array_of_M.size() ; i++ ) 
-			System.out.println(array_of_M.get(i));				
+		
+		Arrays.sort(arr);
+		
+		int gcdVal = arr[1] - arr[0];
+		
+		for (int i = 2 ; i < N ; i++) {
+			gcdVal = gcd(gcdVal, arr[i] - arr[i-1]);
+		}
+		
+		for (int i = 2 ; i <= gcdVal ; i++) {
+			if (gcdVal % i == 0) {
+				System.out.print(i + " ");
+			}
+		}
 	}
+	
+	public static int gcd(int p, int q) {
+		if (q == 0) return p;
+		return gcd(q, p % q);
+	}
+	
 }
