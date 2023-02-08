61661d0c3850a10b26b475c24e1e3969ced499fe
Sangmin Ju <besforyou2@gmail.com>
2023-02-08 14:24:36 +0900

    11660. 구간 합 구하기 5 (Java)

--- 11660/Main.java
+++ 11660/Main.java
@@ -1,26 +1,45 @@
-import java.util.Scanner;
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main (String [] args) {
-
-		Scanner sc = new Scanner(System.in);
+	public static int mat[][];
+	public static int dp[][];
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
 		
-		final int MAX = 1025;
-		int N, M;
-		int x1,y1,x2,y2;	
-
-		int mat[][] = new int[MAX][MAX];	
-
-		N = sc.nextInt();
-		M = sc.nextInt();
-
-		for ( int i = 1 ; i <= N ; i++ ) {
-			for ( int j = 1 ; j <= N ; j++ ) {
-				mat[i][j] = sc.nextInt();
-				mat[i][j] += ( mat[i-1][j-1]
-
-		for ( int i = 1 ; i <= M ; i++ ) {
-			x1 = sc.nextInt(); y1 = sc.nextInt(); 
-			x2 = sc.nextInt(); y2 = sc.nextInt();
-
-				
+		mat = new int[N + 1][N + 1];
+		dp = new int[N + 1][N + 1];
+		
+		for (int i = 1 ; i <= N ; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 1 ; j <= N ; j++) {
+				mat[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		for (int r = 1 ; r <= N ; r++) {
+			for (int c = 1 ; c <= N ; c++) {
+				dp[r][c] = mat[r][c] + dp[r-1][c] + dp[r][c-1] - dp[r-1][c-1];
+			}
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0 ; i < M ; i++) {
+			st = new StringTokenizer(br.readLine());
+			int x1 = Integer.parseInt(st.nextToken());
+			int y1 = Integer.parseInt(st.nextToken());
+			int x2 = Integer.parseInt(st.nextToken());
+			int y2 = Integer.parseInt(st.nextToken());
+			int ans = dp[x2][y2] - dp[x1 - 1][y2] - dp[x2][y1 - 1] + dp[x1 - 1][y1 - 1];
+			sb.append(ans).append("\n");
+		}
+		System.out.println(sb);
+	}
+}
