fc777b3b36fed23b9b3ae17c9a0a7f8b47105628
Sangmin Ju <besforyou2@gmail.com>
2023-02-10 14:36:47 +0900

    11723. 집합 (Java)

---
+++ 11723/Main.java
@@ -0,0 +1,51 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static int M;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		M = Integer.parseInt(br.readLine());
+		
+		int S = 0;
+		
+		StringBuilder sb = new StringBuilder();
+		
+		for (int i = 0 ; i < M ; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int value = 0;
+			String cmd = st.nextToken();
+			
+			switch(cmd) {
+			case "add" :
+				value = Integer.parseInt(st.nextToken());
+				S |= (1 << (value - 1));
+				break;
+			case "remove":
+				value = Integer.parseInt(st.nextToken());
+				S = S & ~(1 << (value - 1));
+				break;
+			case "check":
+				value = Integer.parseInt(st.nextToken());
+				sb.append( (S & (1 << (value - 1))) != 0 ? "1\n" : "0\n");
+				break;
+			case "toggle":
+				value = Integer.parseInt(st.nextToken());
+				S ^= (1 << (value - 1));
+				break;
+			case "all":
+				S |= (~0);
+				break;
+			case "empty":
+				S &= 0;
+				break;
+			}
+		}
+		
+		System.out.println(sb);
+	}
+}
