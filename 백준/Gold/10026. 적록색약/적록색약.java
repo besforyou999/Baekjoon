b9fe32d66e9d08311a91a52f810d35541d4c8395
Sangmin Ju <besforyou2@gmail.com>
2023-02-24 14:16:08 +0900

    [Gold V] Title: 적록색약, Time: 84 ms, Memory: 12672 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -1,6 +1,32 @@
 import java.io.*;
 import java.util.StringTokenizer;
 
+/**
+ * [키워드]
+ * 그래프 탐색 - bfs
+ * 같은 색상이 상하좌우로 인접해 있는 경우 같은 구역  
+ * 
+ * [풀이과정]
+ * 1. 각 위치 방문했는지 안했는지 판별 가능하도록 N * M 2차원 boolean 배열 선언
+ * 2. 모든 좌표를 한번씩 참조하여 아직 방문되지 않은 경우 bfs 탐색
+ * 3. 처음 방문한 좌표 상하좌우를 참조하여 같은 색상인지 확인 -> 같은 색상인 경우 bfs 탐색
+ * 4. bfs 탐색 횟수 = 구역의 개수
+ * 5. 적록색약인 사람의 경우, 원본 그림에서 R를 G로만 바꾸어 2,3,4 과정 실행
+ * 6. 정답 출력
+ *
+ * <p>
+ * [입력]
+ * N x N (1 ≤ N ≤ 100)
+ * <p>
+ * [출력]
+ * 적록색약이 아닌 사람이 봤을 때의 구역 수 , 적록색약인 사람이 봤을 떄의 구역 수
+ *
+ * @author 주상민
+ * @performance 12,860kb, 88ms
+ * @category #그래프#bfs
+ * @see https://www.acmicpc.net/problem/10026
+ */
+
 public class Main {
     static int N;
     static char mat[][];
