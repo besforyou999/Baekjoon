1394861d70835689303b8f6321a57b9a2c9a94c9
Sangmin Ju <besforyou2@gmail.com>
2023-03-14 23:22:12 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 76 ms, Memory: 11404 KB -BaekjoonHub

---
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int dp [][] = new int[16][16];
+
+        for (int room = 1 ; room <= 15 ; room++) {
+            dp[0][room] = room;
+            dp[room - 1][1] = 1;
+        }
+
+        for (int level = 1 ; level <= 14 ; level++) {
+            for (int room = 2 ; room <= 14 ; room++) {
+                dp[level][room] = dp[level][room - 1] + dp[level - 1][room];
+            }
+        }
+        /*
+        for (int level = 14 ; level >= 0 ; level--) {
+            for (int room = 1 ; room <= 14 ; room++) {
+                System.out.print(dp[level][room] + " ");
+            }
+            System.out.println();
+        }
+        */
+
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        while( T-- > 0) {
+            int k = Integer.parseInt(br.readLine());
+            int n = Integer.parseInt(br.readLine());
+            sb.append(dp[k][n]).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
