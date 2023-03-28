4bcb5db1a1821b62d0fac5218bf0c9126ae5badd
Sangmin Ju <besforyou2@gmail.com>
2023-03-28 14:11:07 +0900

    [Silver III] Title: 1로 만들기, Time: 96 ms, Memory: 15516 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,23 +1,28 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+    static int N;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
 
         if (N <= 2) {
-            System.out.println(N -1);
+            System.out.println(N - 1);
             return;
         }
 
         int dp [] = new int[N + 1];
-
-        for (int i = 1 ; i <= N ; i++) {
+        for (int i = 2 ; i <= N ; i++) {
             dp[i] = dp[i-1] + 1;
-            if (i % 3 == 0) dp[i] = Math.min(dp[i], dp[i / 3] + 1);
-            if (i % 2 == 0) dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+
+            if (i % 3 == 0) dp[i] = Math.min(dp[i], dp[i/3] + 1);
+
+            if (i % 2 == 0) dp[i] = Math.min(dp[i], dp[i/2] + 1);
         }
 
-        System.out.print(dp[N] - 1);
+        System.out.println(dp[N]);
     }
 }
