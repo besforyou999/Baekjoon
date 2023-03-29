f2abf2348049b0b84de32a52911a21a0e1b3c30a
Sangmin Ju <besforyou2@gmail.com>
2023-03-29 15:48:45 +0900

    [Silver I] Title: 스티커, Time: 784 ms, Memory: 115036 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.java
+++ 백준/Silver/9465. 스티커/스티커.java
@@ -1,47 +1,37 @@
-import java.util.Scanner;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
+    static int T;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        T = Integer.parseInt(br.readLine());
 
-    public static int Bigger (int a, int b) {
-        if ( a > b )
-            return a;
-        else
-            return b;
-    }
+        int arr [][] = new int[2][100001];
+        int dp [][] = new int[2][100001];
 
-    public static void main(String [] args) {
+        while (T-- > 0) {
 
-        Scanner sc = new Scanner(System.in);
+            int n = Integer.parseInt(br.readLine());
 
-        int t, n;
-        int mat[][] = new int[2][100001];
-        int dp[][] = new int[2][100001];
-
-        t = sc.nextInt();
-
-        while ( t-- > 0 ) {
-
-            n = sc.nextInt();
-
-            for (int i = 1; i <= n ; i++) {
-                mat[0][i] = sc.nextInt();
-            }
-
-            for (int i = 1; i <= n ; i++) {
-                mat[1][i] = sc.nextInt();
+            for (int i = 0 ; i < 2 ; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                for (int j = 1 ; j <= n ; j++)
+                    arr[i][j] = Integer.parseInt(st.nextToken());
             }
 
             dp[0][0] = dp[1][0] = 0;
-            dp[0][1] = mat[0][1];
-            dp[1][1] = mat[1][1];
+            dp[0][1] = arr[0][1];
+            dp[1][1] = arr[1][1];
 
-            for (int j = 2; j <= n ; j++) {
-                dp[0][j] = Bigger(dp[1][j-1], dp[1][j-2]) + mat[0][j];
-                dp[1][j] = Bigger(dp[0][j-1], dp[0][j-2]) + mat[1][j];
+            for (int j = 2 ; j <= n ; j++) {
+                dp[0][j] = Math.max(dp[1][j-1], dp[1][j-2]) + arr[0][j];
+                dp[1][j] = Math.max(dp[0][j-1], dp[0][j-2]) + arr[1][j];
             }
 
-            int ans = Bigger(dp[0][n], dp[1][n]);
-
+            int ans = Math.max(dp[0][n], dp[1][n]);
             System.out.println(ans);
         }
 
