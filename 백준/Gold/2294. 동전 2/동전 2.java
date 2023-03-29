4595865d82b44828a99fb1576712f5542144a7a9
Sangmin Ju <besforyou2@gmail.com>
2023-03-29 12:50:45 +0900

    [Gold V] Title: 동전 2, Time: 112 ms, Memory: 12492 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.java
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -1,38 +1,59 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int n, k;
-    static int[] arr;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
-        arr = new int[N + 1];
 
-        for (int i = 1 ; i <= N ; i++) {
-            st = new StringTokenizer(br.readLine());
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;
 
-        int [] dp = new int[K + 1];
+	static int n, m;
+	static int coins[];
+	
+	static boolean flag = false;
 
-        for (int i = 1 ; i <= K ; i++) {
-            dp[i] = Integer.MAX_VALUE - 1;
-        }
-
-        for (int i = 1 ; i <= N ; i++) {
-            for (int j = arr[i]; j <= K ; j++) {
-                dp[j] = Math.min(dp[j], dp[j-arr[i]] + 1);
-            }
-        }
-
-        if (dp[K] == Integer.MAX_VALUE - 1) {
-            System.out.println(-1);
-        } else
-            System.out.println(dp[K]);
-    }
+	public static void main(String[] args) throws IOException {
+		input();
+		bfs();
+		if (!flag) {
+			System.out.println("-1");
+		}
+	}
+	
+	public static void bfs() {
+		Queue<int []> qu = new ArrayDeque<>();
+		boolean visited[] = new boolean [m + 1];
+		qu.add(new int [] {0,1});
+		visited[0] = true;
+		while (!qu.isEmpty()) {
+			int now[] = qu.poll();
+			for (int i = 0; i < n; i++) {
+				int coin = coins[i] + now[0];
+				if (coin > m || visited[coin]) {
+					continue;
+				}
+				if (coin == m) {
+					System.out.println(now[1]);
+					flag = true;
+					return;
+				}
+				visited[coin] = true;
+				qu.add(new int[] {coin, now[1] + 1});
+			}
+		}
+	}
+	
+	public static void input() throws IOException {
+		st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		coins = new int [n];
+		for (int i = 0; i < n; i++) {
+			coins[i] = Integer.parseInt(br.readLine());
+		}
+	}
 }
