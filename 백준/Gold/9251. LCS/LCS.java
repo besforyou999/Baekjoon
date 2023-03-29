af7942ca0f1ab75fc14a2b35081a93c6203aa6a0
Sangmin Ju <besforyou2@gmail.com>
2023-03-29 15:29:06 +0900

    [Gold V] Title: LCS, Time: 104 ms, Memory: 15672 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.java
+++ 백준/Gold/9251. LCS/LCS.java
@@ -1,20 +1,22 @@
-import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Main {
+    static char [] line1;
+    static char [] line2;
+    static int dp[][];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        char [] line1 = br.readLine().toCharArray();
-        char [] line2 = br.readLine().toCharArray();
+        String a = br.readLine();
+        String b = br.readLine();
+        line1 = a.toCharArray();
+        line2 = b.toCharArray();
 
-        int len1 = line1.length;
-        int len2 = line2.length;
+        dp = new int[line1.length + 1][line2.length + 1];
 
-        int [][] dp = new int[len1 + 1][len2 + 1];
-
-        for (int i = 1 ; i <= len1 ; i++) {
-            for (int j =1 ; j <= len2 ; j++) {
+        for (int i = 1 ; i <= line1.length ; i++) {
+            for (int j = 1 ; j <= line2.length ; j++) {
                 if (line1[i-1] != line2[j-1]) {
                     dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
                 } else {
@@ -23,6 +25,6 @@ public class Main {
             }
         }
 
-        System.out.print(dp[len1][len2]);
+        System.out.println(dp[line1.length][line2.length]);
     }
 }
