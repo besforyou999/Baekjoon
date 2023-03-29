d5303c216aa2c206cd6ddb2edd7c580aafd9589c
Sangmin Ju <besforyou2@gmail.com>
2023-03-29 12:42:02 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 76 ms, Memory: 11520 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
@@ -1,36 +1,36 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    static int N;
     static final long MOD = 1_000_000_000L;
-    public static void main(String args[]) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
 
-        N = Integer.parseInt(bf.readLine());
+        long[][] dp = new long[N + 1][10];
 
-        long[][] dp = new long[N+1][10];
-
-        // init
-        for(int i=0; i<=9; i++) {
+        for (int i = 0 ; i < 10 ; i++) {
             dp[1][i] = 1L;
         }
 
-        for(int i=2; i<=N; i++) {
-            // 0으로 시작하는 수의 개수
+        for (int i = 2 ; i <= N ; i++) {
             dp[i][0] = dp[i-1][1];
-            for(int j=1; j<=9; j++) {
-                // 9일경우는 이전 자릿수의 8로 시작하는 수의 개수와 동일하다!
-                if(j == 9) dp[i][9] = dp[i-1][8] % MOD;
-                // 나머지 수들은 이전 숫자의 j-1로 시작하는 수의 개수와 j+1로 시작하는 수의 개수를 더한 값이다.
-                else dp[i][j] = (dp[i-1][j-1] % MOD + dp[i-1][j+1] % MOD) % MOD;
+            for (int j = 1 ; j < 10 ; j++) {
+                if (j == 9) {
+                    dp[i][j] = dp[i-1][8] % MOD;
+                } else {
+                    dp[i][j] = (dp[i-1][j-1] % MOD + dp[i-1][j+1] % MOD) % MOD;
+                }
             }
         }
 
         long result = 0;
-        for(int i=1; i<=9; i++) {
-            result = (result + dp[N][i]) % MOD;    // 정답을 더하면서 오버플로우가 발생할 수 있으므로 주의!
+
+        for (int j = 1 ; j < 10 ; j++) {
+            result = (result + dp[N][j]) % MOD;
         }
+
         System.out.print(result);
     }
 }
