73f1f167c1f238e716fe174c88244a46838d8074
Sangmin Ju <besforyou2@gmail.com>
2023-03-29 10:57:55 +0900

    [Silver II] Title: 이항 계수 2, Time: 124 ms, Memory: 16196 KB -BaekjoonHub

---
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;
+	
+	static int n, m;
+	static int dp[][];
+	public static void main(String[] args) throws IOException {
+		st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		dp = new int [n + 1][n + 1];
+		for (int i = 0; i < n + 1; i++) {
+			for (int j = 0; j < n + 1; j++) {
+				dp[i][j] = -1;
+			}
+		}
+		recur(0,0);
+		System.out.println(dp[0][0] % 10007);
+	}
+
+	public static int recur(int times, int got) {
+		if (times == n) {
+			if (got == m) {
+				return 1;
+			}
+			return 0;
+		}
+		if (dp[times][got] != -1) {
+			return dp[times][got] % 10007;
+		}
+		dp[times][got] = recur(times + 1, got) % 10007 + recur(times + 1, got + 1) % 10007;
+		return dp[times][got] % 10007;
+	}
+}
