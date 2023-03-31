a220ae2acc3ecefc25c9ea78cedf817b63780ac0
Sangmin Ju <besforyou2@gmail.com>
2023-03-31 10:49:06 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 96 ms, Memory: 12084 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,37 +1,38 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N;
-    static int A[], dp[];
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        A = new int[N + 1];
-        dp = new int[N + 1];
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 1 ; i <= N ; i++) {
-            A[i] = Integer.parseInt(st.nextToken());
-        }
+	static int n, m;
 
-        for (int i = 1 ; i <= N ; i++) {
-            dp[i] = 1;
-            for (int j = 1 ; j <= i ; j++) {
-                if (A[i] > A[j] && dp[i] < dp[j] + 1) {
-                    dp[i] = dp[j] + 1;
-                }
-            }
-        }
+	public static void main(String[] args) throws IOException {
+		int n = Integer.parseInt(br.readLine());
+		int arr[] = new int[n];
+		int lis[] = new int[n];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
 
-        int max = -1;
-
-        for (int i = 1 ; i <= N ; i++) {
-            if (dp[i] > max) max = dp[i];
-        }
-
-        System.out.println(max);
-    }
+		int max = -2147483648;
+		for (int i = 0; i < n; i++) {
+			lis[i] = 1;
+			for (int j = 0; j < i; j++) {
+				if (arr[j] < arr[i]) {
+					lis[i] = Math.max(lis[i], lis[j] + 1);
+				}
+			}
+			max = Math.max(max, lis[i]);
+		}
+		System.out.println(max);
+	}
 }
