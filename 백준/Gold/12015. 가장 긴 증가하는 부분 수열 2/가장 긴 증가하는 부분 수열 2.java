69167c2318578f44b77fa4bb7a28692854eba6e1
Sangmin Ju <besforyou2@gmail.com>
2023-03-31 14:34:30 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 484 ms, Memory: 125340 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -1,39 +1,40 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
+	static int n, m;
+	static int lis[];
 
-        ArrayList<Integer> ans = new ArrayList<>();
-        ans.add(-1);
+	public static void main(String[] args) throws IOException {
+		int n = Integer.parseInt(br.readLine());
+		int arr[] = new int[n];
+		lis = new int [n];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+			lis[i] = 2147483647;
+		}
+		lis[0] = arr[0];
+		int index = 1;
+		for (int i = 1; i < n; i++) {
+			if (lis[index - 1] < arr[i]) {
+				lis[index++] = arr[i];
+			} else {
+				int value = Arrays.binarySearch(lis, arr[i]);
+				if (value < 0) {
+					value = (value + 1) * -1; 
+				}
+				lis[value] = arr[i];
+			}
+		}
+		System.out.println(index);
+	}
 
-        for (int i = 0 ; i < N ; i++) {
-            int val = Integer.parseInt(st.nextToken());
-            int len = ans.size();
-            if (val > ans.get(len - 1)) {
-                ans.add(val);
-            } else {
-                int lp = 1, rp = len - 1;
-                while (lp < rp) {
-                    int mid = (lp + rp) / 2;
-                    if (ans.get(mid) < val) {
-                        lp = mid + 1;
-                    } else {
-                        rp = mid;
-                    }
-                }
-                ans.set(rp, val);
-            }
-        }
-
-        System.out.println(ans.size() - 1);
-    }
 }
