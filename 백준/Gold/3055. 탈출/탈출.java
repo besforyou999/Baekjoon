60539e156ff59dc6297e967d279bbf3ec64cd1bd
Sangmin Ju <besforyou2@gmail.com>
2023-04-03 16:39:30 +0900

    [Gold IV] Title: 탈출, Time: 84 ms, Memory: 11840 KB -BaekjoonHub

--- 백준/Gold/3055. 탈출/탈출.java
+++ 백준/Gold/3055. 탈출/탈출.java
@@ -1,117 +1,103 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import sun.awt.image.ImageWatched;
+
+import java.io.*;
+import java.util.*;
 
 public class Main {
+    static int R, C;
+    static char [][] mat;
+    static int dr[] = {-1, 1, 0, 0};
+    static int dc[] = {0, 0, -1, 1};
 
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringBuilder sb = new StringBuilder();
-	static StringTokenizer st;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        mat = new char[R][C];
 
-	static char map[][];
-	static boolean visited[][];
+        ArrayDeque<Node> water = new ArrayDeque<>();
+        ArrayDeque<Node> animal = new ArrayDeque<>();
 
-	static int dx[] = new int[] { 1, 0, -1, 0 };
-	static int dy[] = new int[] { 0, 1, 0, -1 };
+        for (int r = 0 ; r < R ; r++) {
+            String line = br.readLine();
+            for (int c = 0 ; c < C ; c++) {
+                mat[r][c] = line.charAt(c);
+                if (mat[r][c] == 'S') {
+                    animal.add(new Node(mat[r][c], r, c, 0));
+                } else if (mat[r][c] == '*') {
+                    water.add(new Node(mat[r][c], r, c, 0));
+                }
+            }
+        }
 
-	static int n, m;
-	static int answer = 0;
-	static boolean flag = true;
-	static boolean flag2 = true;
+        int result = bfs(water, animal);
 
-	static Queue<int[]> water;
-	static Queue<int[]> dochi;
+        if (result == -1) {
+            System.out.println("KAKTUS");
+        } else {
+            System.out.println(result);
+        }
 
-	public static void main(String[] args) throws IOException {
-		input();
-		while (flag) {
-			waterbfs();
-			dochibfs();
-			if (flag2 == false) {
-				System.out.println("KAKTUS");
-				return;
-			}
-		}
-		System.out.println(answer);
-	}
+    }
 
-	public static void waterbfs() {
-		int size = water.size();
-		while (size-- > 0) {
-			int now[] = water.poll();
-			for (int i = 0; i < 4; i++) {
-				int nexty = now[0] + dy[i];
-				int nextx = now[1] + dx[i];
-				if (nexty < 0 || nexty > n - 1 || nextx < 0 || nextx > m - 1) {
-					continue;
-				}
-				if (visited[nexty][nextx] || map[nexty][nextx] == '*' || map[nexty][nextx] == 'X' || map[nexty][nextx] == 'D') {
-					continue;
-				}
-				visited[nexty][nextx] = true;
-				map[nexty][nextx] = '*';
-				water.add(new int [] {nexty, nextx});
-			}
-		}
-	}
-	
-	public static void dochibfs() {
-		int size = dochi.size();
-		if (size == 0) {
-			flag2 = false;
-			return;
-		}
-		while (size-- > 0) {
-			int now[] = dochi.poll();
-			for (int i = 0; i < 4; i++) {
-				int nexty = now[0] + dy[i];
-				int nextx = now[1] + dx[i];
-				if (nexty < 0 || nexty > n - 1 || nextx < 0 || nextx > m - 1) {
-					continue;
-				}
-				if (visited[nexty][nextx] || map[nexty][nextx] == '*' || map[nexty][nextx] == 'X') {
-					continue;
-				}
-				if (map[nexty][nextx] == 'D') {
-					flag = false;
-					answer = now[2];
-					return;
-				}
-				visited[nexty][nextx] = true;
-				dochi.add(new int [] {nexty, nextx, now[2] + 1});
-			}
-		}
-	}
+    static int bfs(ArrayDeque<Node> water, ArrayDeque<Node> animal) {
 
-	public static void input() throws IOException {
-		st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
+        while (!(water.isEmpty() && animal.isEmpty())) {
+            int size = water.size();
 
-		map = new char[n][m];
-		visited = new boolean[n][m];
-		
-		water = new ArrayDeque<>();
-		dochi = new ArrayDeque<>();
+            while(size-- > 0) {
+                Node node = water.poll();
 
-		for (int i = 0; i < n; i++) {
-			String str = br.readLine();
-			for (int j = 0; j < m; j++) {
-				map[i][j] = str.charAt(j);
-				if (str.charAt(j) == '*') {
-					water.add(new int[] { i, j });
-					visited[i][j] = true;
-				}
-				if (str.charAt(j) == 'S') {
-					dochi.add(new int[] { i, j, 1 });
-					visited[i][j] = true;
-				}
-			}
-		}
+                for (int i = 0; i < 4; i++) {
+                    int nr = node.r + dr[i];
+                    int nc = node.c + dc[i];
+                    if (isIn(nr, nc)) {
+                        if (mat[nr][nc] == '.' || mat[nr][nc] == 'S') {
+                            mat[nr][nc] = '*';
+                            water.add(new Node(node.ch, nr, nc, node.cnt));
+                        }
+                    }
+                }
+            }
 
-	}
+            size = animal.size();
 
+            while(size-- > 0) {
+                Node node = animal.poll();
+
+                for (int i = 0 ; i < 4 ; i++) {
+                    int nr = node.r + dr[i];
+                    int nc = node.c + dc[i];
+                    if (isIn(nr, nc)) {
+                        if (mat[nr][nc] == '.') {
+                            mat[nr][nc] = 'S';
+                            animal.add(new Node(node.ch, nr, nc, node.cnt + 1));
+                        } else if (mat[nr][nc] == 'D') {
+                            return node.cnt + 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    static boolean isIn(int r, int c) {
+        return (r >= 0 && r < R && c >= 0 && c < C);
+    }
+}
+
+
+class Node {
+    char ch;
+    int r, c, cnt;
+    Node(char letter, int r, int c, int cnt) {
+        this.ch = letter;
+        this.r = r;
+        this.c = c;
+        this.cnt = cnt;
+    }
 }
