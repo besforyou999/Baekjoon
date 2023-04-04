5fe149415915aa659d3983ffe1a64d2b3117f78b
Sangmin Ju <besforyou2@gmail.com>
2023-04-04 14:27:40 +0900

    [Gold II] Title: 미네랄, Time: 504 ms, Memory: 40940 KB -BaekjoonHub

--- 백준/Gold/2933. 미네랄/미네랄.java
+++ 백준/Gold/2933. 미네랄/미네랄.java
@@ -2,158 +2,189 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
+import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int R, C;
-    static char [][] cave;
 
-    static int dr [] = {-1, 1, 0, 0};
-    static int dc [] = {0, 0, -1, 1};
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        R = Integer.parseInt(st.nextToken());
-        C = Integer.parseInt(st.nextToken());
+	static int n, m, c, xcount, findxcount;
+	static char map[][];
+	static boolean visited[][];
+	
+	static int dx[] = new int[] {1, 0, -1, 0};
+	static int dy[] = new int[] {0, 1, 0, -1};
+	
+	static PriorityQueue<int[]> keepqu;
+	static PriorityQueue<int[]> mapupdate;
 
-        cave = new char[R + 1][C + 1];
+	public static void main(String[] args) throws IOException {
+		input();
+		c = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
+		for (int i = 1; i <= c; i++) {
+			if (i % 2 == 1) {
+				destroy(true);
+			} else {
+				destroy(false);
+			}
+			gravity();
+		}
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				sb.append(map[i][j]);
+			}
+			sb.append("\n");
+		}
+		System.out.println(sb);
+	}
+	
+	public static void destroy(boolean flag) {
+		int floor = n - Integer.parseInt(st.nextToken());
+		if (flag) {
+			for (int i = 0; i < m; i++) {
+				if (map[floor][i] == 'x') {
+					map[floor][i] = '.';
+					xcount--;
+					return;
+				}
+			}
+		} else {
+			for (int i = m - 1; i >= 0; i--) {
+				if (map[floor][i] == 'x') {
+					map[floor][i] = '.';
+					xcount--;
+					return;
+				}
+			}
+		}
+	}
+	
+	public static void gravity() {
+		if (!find_xcount()) {
+			go_gravity();
+		}
+	}
+	
+	public static boolean find_xcount() {
+		visited = new boolean[n][m];
+		findxcount = 0;
+		for (int i = 0; i < m; i++) {
+			if (map[n - 1][i] == 'x' && !visited[n - 1][i]) {
+				bfs(n - 1, i);
+				if (findxcount == xcount) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
+	public static void bfs(int y, int x) {
+		Queue<int[]> qu = new ArrayDeque<>();
+		qu.add(new int [] {y, x});
+		visited[y][x] = true;
+		findxcount++;
+		while (!qu.isEmpty()) {
+			int now[] = qu.poll();
+			int nexty,nextx;
+			for (int i = 0; i < 4; i++) {
+				nexty = now[0] + dy[i];
+				nextx = now[1] + dx[i];
+				if (nexty < 0 || nexty > n - 1 || nextx < 0 || nextx > m - 1) {
+					continue;
+				}
+				if (visited[nexty][nextx] || map[nexty][nextx] == '.') {
+					continue;
+				}
+				visited[nexty][nextx] = true;
+				qu.add(new int [] {nexty, nextx});
+				findxcount++;
+			}
+		}
+	}
 
-        for (int r = 1 ; r <= R ; r++) {
-            String line = br.readLine();
-            for (int c = 1 ; c <= C ; c++) {
-                cave[r][c] = line.charAt(c - 1);
-            }
-        }
+	public static void go_gravity() {
+		keepqu = new PriorityQueue<>((o1, o2) -> o2[0] - o1[0]);
+		mapupdate = new PriorityQueue<>((o1, o2) -> o2[0] - o1[0]);
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				if (!visited[i][j] && map[i][j] == 'x') {
+					bfsdown(i, j);
+				}
+			}
+		}
+		char tempmap[][] = new char [n][m];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				tempmap[i][j] = map[i][j];
+			}
+		}
+		
+		loop : while (!keepqu.isEmpty()) {
+			PriorityQueue<int[]> keep2 = new PriorityQueue<>((o1, o2) -> o2[0] - o1[0]);
+			int size = keepqu.size();
+			while (size-- > 0) {
+				int now[] = keepqu.poll();
+				if (now[0] == n - 1 || tempmap[now[0] + 1][now[1]] == 'x') {
+					break loop;
+				}
+				tempmap[now[0]][now[1]] = '.';
+				tempmap[now[0] + 1][now[1]] = 'x';
+				keep2.add(new int [] {now[0] + 1, now[1]});
+			}
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < m; j++) {
+					map[i][j] = tempmap[i][j];
+				}
+			}
+			keepqu = keep2;
+		}
+	}
 
-        int N = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine());
-
-        int dir = 1; // 1 : left, -1 : right
-        for (int n = 0 ; n < N ; n++) {
-            int height = Integer.parseInt(st.nextToken());
-            if (dir == 1) {
-                shootArrowFromLeft(height);
-            } else
-                shootArrowFromRight(height);
-
-            dropClusterIfExist();
-
-            dir *= -1;
-        }
-
-        printMat();
-
-    }
-
-    static void shootArrowFromLeft(int height) {
-        int row = R - height + 1;
-        for (int col = 1 ; col <= C ; col++) {
-            if (cave[row][col] == 'x') {
-                cave[row][col] = '.';
-                return;
-            }
-        }
-    }
-
-    static void shootArrowFromRight(int height) {
-        int row = R - height + 1;
-        for (int col = C ; col >= 1 ; col--) {
-            if (cave[row][col] == 'x') {
-                cave[row][col] = '.';
-                return;
-            }
-        }
-    }
-
-    static void printMat() {
-        StringBuilder sb = new StringBuilder();
-        for (int r = 1 ; r <= R ; r++) {
-            for (int c = 1 ; c <= C ; c++) {
-                sb.append(cave[r][c]);
-            }
-            sb.append("\n");
-        }
-        System.out.print(sb);
-    }
-
-    static void dropClusterIfExist() {
-        boolean [][] connectedToGround = new boolean[R + 1][C + 1];
-        for (int c = 1 ; c <= C ; c++) {
-            if (cave[R][c] == 'x' && !connectedToGround[R][c]) {
-                bfs(connectedToGround, R, c);
-            }
-        }
-        ArrayDeque<int[]> cluster = new ArrayDeque<>();
-        ArrayDeque<int[]> clusterBottom = new ArrayDeque<>();
-
-        for (int r = 1 ; r <= R ; r++) {
-            for (int c = 1 ; c <= C ; c++) {
-                if (cave[r][c] == 'x' && !connectedToGround[r][c]) {
-                    cluster.add(new int[] {r, c});
-                    if (isIn(r + 1, c) && cave[r+1][c] == '.') {
-                        clusterBottom.add(new int[] {r, c});
-                    }
-                }
-            }
-        }
-
-        if (cluster.isEmpty()) return; // 공중에 떠있는 클러스터가 없으면 그대로 종료
-
-        // 떨어질 높이 계산. 클러스터 바닥과 x 또는 지면과의 최단 거리가 떨어질 높이
-
-        int fallHeight = 10000;
-
-        for (int [] coord : clusterBottom) {
-            int r = coord[0];
-            int c = coord[1];
-            int height = 0;
-            for (int row = r + 1 ; row <= R ; row++) {
-                if (cave[row][c] == 'x' && connectedToGround[row][c]) {
-                    fallHeight = Math.min(fallHeight, height);
-                    break;
-                } else if (cave[row][c] == '.' && row == R) {
-                    fallHeight = Math.min(fallHeight, height + 1);
-                    break;
-                }
-                height++;
-            }
-        }
-
-        for (int [] coord : cluster) {
-            int r = coord[0];
-            int c = coord[1];
-            cave[r][c] = '.';
-        }
-
-        for (int [] coord : cluster) {
-            int r = coord[0] + fallHeight;
-            int c = coord[1];
-            cave[r][c] = 'x';
-        }
-
-    }
-
-    static void bfs(boolean [][] connectedToGround, int r, int c) {
-        connectedToGround[r][c] = true;
-        ArrayDeque<int[]> deque = new ArrayDeque<>();
-        deque.add(new int[] {r, c});
-
-        while(!deque.isEmpty()) {
-            int [] coord = deque.poll();
-            for (int i = 0 ; i < 4 ; i++) {
-                int nr = coord[0] + dr[i];
-                int nc = coord[1] + dc[i];
-                if (isIn(nr, nc) && cave[nr][nc] == 'x' && !connectedToGround[nr][nc]) {
-                    connectedToGround[nr][nc] = true;
-                    deque.add(new int[] {nr, nc});
-                }
-            }
-        }
-    }
-
-    static boolean isIn(int r, int c) {
-        return (r >= 1 && r <= R && c >= 1 && c <= C);
-    }
+	public static void bfsdown(int y, int x) {
+		visited[y][x] = true;
+		Queue<int[]> qu = new ArrayDeque<>();
+		keepqu.add(new int[] {y,x});
+		qu.add(new int[] {y, x});
+		while (!qu.isEmpty()) {
+			int now[] = qu.poll();
+			int nexty,nextx;
+			for (int i = 0; i < 4; i++) {
+				nexty = now[0] + dy[i];
+				nextx = now[1] + dx[i];
+				if (nexty < 0 || nexty > n - 1 || nextx < 0 || nextx > m - 1) {
+					continue;
+				}
+				if (visited[nexty][nextx] || map[nexty][nextx] == '.') {
+					continue;
+				}
+				visited[nexty][nextx] = true;
+				qu.add(new int [] {nexty, nextx});
+				keepqu.add(new int[] {nexty, nextx});
+			}
+		}
+	}
+	
+	public static void input() throws IOException {
+		st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		map = new char[n][m];
+		xcount = 0;
+		for (int i = 0; i < n; i++) {
+			String str = br.readLine();
+			for (int j = 0; j < m; j++) {
+				map[i][j] = str.charAt(j);
+				if (str.charAt(j) == 'x') {
+					xcount++;
+				}
+			}
+		}
+	}
 
 }
