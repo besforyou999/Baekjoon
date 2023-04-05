5e1d652f8e2a21b91a40790e61d930e22228728b
Sangmin Ju <besforyou2@gmail.com>
2023-04-05 15:07:23 +0900

    [Gold III] Title: 경사로, Time: 104 ms, Memory: 12692 KB -BaekjoonHub

--- 백준/Gold/14890. 경사로/경사로.java
+++ 백준/Gold/14890. 경사로/경사로.java
@@ -1,138 +1,174 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int X = Integer.parseInt(st.nextToken());
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;
 
-        int mat[][] = new int[N + 1][N + 1];
+	static int n, x;
+	static int map[][];
+	static int count;
 
-        for (int i = 1; i <= N; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 1; j <= N; j++) {
-                mat[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
+	public static void main(String[] args) throws IOException {
+		input();
+		left_right();
+		up_down();
+		System.out.println(count);
+	}
 
-        int cnt = 0;
+	public static void left_right() {
+		boolean visited[][] = new boolean[n][n];
+		for (int i = 0; i < n; i++) {
+			boolean c = true;
+			loop: for (int j = 0; j < n - 1; j++) {
+				int value = map[i][j] - map[i][j + 1];
+				if (value == 0 || visited[i][j + 1]) {
+					continue;
+				} else if (value == 1) { // 내려감
+					for (int d = 1; d < x; d++) {
+						if (j + 1 + d > n - 1) {
+							c = false;
+							break loop;
+						}
+						if (visited[i][j + 1 + d]) {
+							c = false;
+							break loop;
+						}
+						if (map[i][j + 1] != map[i][j + 1 + d]) {
+							c = false;
+							break loop;
+						}
+					}
+					if (visited[i][j + 1] == true) {
+						c = false;
+						break;
+					}
+					visited[i][j + 1] = true;
+					for (int d = 1; d < x; d++) {
+						visited[i][j + 1 + d] = true;
+					}
+					c = true;
+				} else if (value == -1) { // 올라감
+					for (int d = 1; d < x; d++) {
+						if (j - d < 0) {
+							c = false;
+							break loop;
+						}
+						if (visited[i][j - d]) {
+							c = false;
+							break loop;
+						}
+						if (map[i][j] != map[i][j - d]) {
+							c = false;
+							break loop;
+						}
+					}
+					if (visited[i][j] == true) {
+						c = false;
+						break;
+					}
+					visited[i][j] = true;
+					for (int d = 1; d < x; d++) {
+						visited[i][j - d] = true;
+					}
+					c = true;
+				} else {
+					c = false;
+					break;
+				}
+			}
+			if (c) {
+				count++;
+			}
+		}
+	}
 
-        // 행 탐색
-        for (int row = 1; row <= N; row++) {
-            boolean goodLine = true;
-            boolean[] used = new boolean[N + 1];
-            int prevHeight = mat[row][1];
+	public static void up_down() {
+		boolean visited[][] = new boolean[n][n];
+		for (int i = 0; i < n; i++) {
+			boolean c = true;
+			loop: for (int j = 0; j < n - 1; j++) {
+				int value = map[j][i] - map[j + 1][i];
+				if (value == 0 || visited[j + 1][i]) {
+					continue;
+				} else if (value == 1) {
+					for (int d = 1; d < x; d++) {
+						if (j + 1 + d > n - 1) {
+							c = false;
+							break loop;
+						}
+						if (visited[j + 1 + d][i]) {
+							c = false;
+							break loop;
+						}
+						if (map[j + 1][i] != map[j + 1 + d][i]) {
+							c = false;
+							break loop;
+						}
+					}
+					if (visited[j + 1][i] == true) {
+						c = false;
+						break;
+					}
+					visited[j + 1][i] = true;
+					for (int d = 1; d < x; d++) {
+						visited[j + 1 + d][i] = true;
+					}
+					c = true;
+				} else if (value == -1) {
+					for (int d = 1; d < x; d++) {
+						if (j - d < 0) {
+							c = false;
+							break loop;
+						}
+						if (visited[j - d][i]) {
+							c = false;
+							break loop;
+						}
+						if (map[j][i] != map[j - d][i]) {
+							c = false;
+							break loop;
+						}
+					}
+					if (visited[j][i] == true) {
+						c = false;
+						break;
+					}
+					visited[j][i] = true;
+					for (int d = 1; d < x; d++) {
+						visited[j - d][i] = true;
+					}
+					c = true;
+				} else {
+					c = false;
+					break;
+				}
+			}
+			if (c) {
+				count++;
+			}
+		}
+	}
 
-            for (int col = 1; col <= N; col++) {
-                int curHeight = mat[row][col];
+	public static void input() throws IOException {
+		st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		x = Integer.parseInt(st.nextToken());
+		map = new int[n][n];
+		count = 0;
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < n; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+	}
 
-                if (prevHeight - 1 == curHeight) {
-
-                    int countSmallHeight = 0;
-                    for (int i = 0; i < X; i++) {
-                        if (col + i <= N && !used[col + i] && mat[row][col + i] == curHeight) {
-                            used[col + i] = true;
-                            countSmallHeight += 1;
-                        }
-                    }
-
-                    if (countSmallHeight < X) {
-                        goodLine = false;
-                        break;
-                    }
-                    col += (X - 1);
-                    prevHeight = curHeight;
-
-                } else if (prevHeight + 1 == curHeight) {
-
-                    int countLowHeight = 0;
-                    for (int i = 0; i < X; i++) {
-                        if (col - i - 1 >= 1 && !used[col - i - 1] && mat[row][col - i - 1] == curHeight - 1) {
-                            used[col - i - 1] = true;
-                            countLowHeight += 1;
-                        }
-                    }
-
-                    if (countLowHeight < X) {
-                        goodLine = false;
-                        break;
-                    }
-
-                    prevHeight = curHeight;
-
-                } else if (Math.abs(prevHeight - curHeight) > 1) {
-                    goodLine = false;
-                    break;
-                }
-            }
-
-            if (goodLine) {
-                //System.out.println("good row : " + row);
-                cnt++;
-            }
-        }
-
-        // 열 탐색
-        for (int col = 1; col <= N; col++) {
-            boolean goodLine = true;
-            boolean[] used = new boolean[N + 1];
-            int prevHeight = mat[1][col];
-
-            for (int row = 1; row <= N; row++) {
-                int curHeight = mat[row][col];
-
-                if (prevHeight - 1 == curHeight) {
-
-                    int countSmallHeight = 0;
-                    for (int i = 0; i < X; i++) {
-                        if (row + i <= N && !used[row + i] && mat[row + i][col] == curHeight) {
-                            used[row + i] = true;
-                            countSmallHeight += 1;
-                        }
-                    }
-
-                    if (countSmallHeight < X) {
-                        goodLine = false;
-                        break;
-                    }
-
-                    row += (X - 1);
-                    prevHeight = curHeight;
-
-                } else if (prevHeight + 1 == curHeight) {
-
-                    int countLowHeight = 0;
-                    for (int i = 0; i < X; i++) {
-                        if (row - i - 1 >= 1 && !used[row - i - 1] && mat[row - i - 1][col] == curHeight - 1) {
-                            used[row - i - 1] = true;
-                            countLowHeight += 1;
-                        }
-                    }
-
-                    if (countLowHeight < X) {
-                        goodLine = false;
-                        break;
-                    }
-
-                    prevHeight = curHeight;
-                } else if (Math.abs(prevHeight - curHeight) > 1) {
-                    goodLine = false;
-                    break;
-                }
-            }
-
-            if (goodLine) {
-                //System.out.println("good col : " + col);
-                cnt++;
-            }
-        }
-
-        System.out.print(cnt);
-    }
 }
