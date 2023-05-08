f9aa349ecb3179572fce309c8b89e69d20af7470
Sangmin Ju <besforyou2@gmail.com>
2023-05-08 22:57:06 +0900

    [Gold I] Title: 수들의 합 7, Time: 1348 ms, Memory: 329932 KB -BaekjoonHub

--- 백준/Gold/2268. 수들의 합 7/수들의 합 7.java
+++ 백준/Gold/2268. 수들의 합 7/수들의 합 7.java
@@ -5,72 +5,75 @@ import java.util.StringTokenizer;
 
 public class Main {
     static int N, M;
-    static int arr[];
-    static long tree[];
+    static long [] arr, tree;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        arr = new int[N + 1];
-        tree = new long[N * 4];
+        int k = (int)Math.ceil((Math.log(N) / Math.log(2))) + 1;
+        int size = (int)Math.pow(2, k);
+
+        arr = new long[N + 1];
+        tree = new long[size];
 
         StringBuilder sb = new StringBuilder();
 
-        for (int m = 0 ; m < M ; m++) {
+        for (int m = 0; m < M ; m++) {
             st = new StringTokenizer(br.readLine());
             int cmd = Integer.parseInt(st.nextToken());
             int i = Integer.parseInt(st.nextToken());
             int j = Integer.parseInt(st.nextToken());
 
-            if (cmd == 0 && i > j) {
-                int tmp = i;
-                i = j;
-                j = tmp;
-            }
-
             if (cmd == 0) {
-                sb.append(sum(1, N, 1, i, j) + "\n");
+                int left = i;
+                int right = j;
+
+                if (left > right) {
+                    int tmp = left;
+                    left = right;
+                    right = tmp;
+                }
+
+                sb.append(sum(1, N, 1, left, right)).append("\n");
             } else {
                 update(1, N, 1, i, j);
             }
+
         }
 
-        System.out.println(sb);
+        System.out.print(sb);
     }
 
-    /*
-        node의 범위 : start, end
-        누적합 범위 : left, right
-     */
     static long sum(int start, int end, int node, int left, int right) {
-        if (right < start || end < left) return 0;
+        if (right < start || end < left) {
+            return 0;
+        }
 
-        if (left <= start && end <= right) return tree[node];
+        if (left <= start && end <= right) {
+            return tree[node];
+        }
 
         int mid = (start + end) / 2;
 
         return sum(start, mid, node * 2, left, right) + sum(mid + 1, end, node * 2 + 1, left, right);
     }
 
-    /*
-        node의 범위 : start, end
-        변경할 인덱스 : index
-        변경할 값 : val
-     */
-
-    static long update (int start, int end, int node, int index, int val) {
-        if (index < start || end < index)
+    static long update(int start, int end, int node, int idx, int number) {
+        if (idx < start || end < idx) {
             return tree[node];
+        }
 
-        if (start == end)
-            return tree[node] = val;
+        if (start == end) {
+            return tree[node] = number;
+        }
 
         int mid = (start + end) / 2;
 
-        return tree[node] = update(start, mid, node * 2, index, val) + update(mid + 1, end, node * 2 + 1, index, val);
+        return tree[node] = update(start, mid, node * 2, idx, number) + update(mid + 1, end, node * 2 + 1, idx, number);
 
     }
+
+
 }
