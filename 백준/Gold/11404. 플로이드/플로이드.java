7f85f9e294576887c31c584454bf1f208d99b52e
Sangmin Ju <besforyou2@gmail.com>
2023-05-22 22:27:43 +0900

    [Gold IV] Title: 플로이드, Time: 340 ms, Memory: 41472 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.java
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -1,67 +1,52 @@
 import java.io.BufferedReader;
-import java.io.IOError;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int n,m;
-    static int mat[][];
-
-    static int INF = 1000000000;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        m = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
 
-        mat = new int[n + 1][n + 1];
-        for (int i = 0 ; i < m ; i++) {
+        int [][]map = new int[N + 1][N + 1];
+
+        for (int m = 0 ; m < M ; m++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            int weight = Integer.parseInt(st.nextToken());
-            if (mat[start][end] == 0) {
-                mat[start][end] = weight;
-            } else {
-                mat[start][end] = Math.min(mat[start][end], weight);
-            }
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            if (map[a][b] == 0)
+                map[a][b] = c;
+            else
+                map[a][b] = Math.min(map[a][b], c);
         }
 
-        int dist [][] = new int[n + 1][n + 1];
-        for (int i = 1 ; i <= n ; i++) {
-            for (int j = 1 ; j <= n ; j++) {
-                if (i == j) {
-                    dist[i][j] = 0;
-                } else if (mat[i][j] == 0) {
-                    dist[i][j] = INF;
-                } else {
-                    dist[i][j] = mat[i][j];
-                }
-            }
-        }
-
-        // 경유지, 출발지, 도착지
-        for (int k = 1 ; k <= n ; k++) {
-            for (int i = 1 ; i <= n ; i++) {
-                for (int j = 1 ; j <= n ; j++) {
-                    if (i == j) continue;
-                    dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+        for (int k = 1 ; k <= N ; k++) {
+            for (int i = 1 ; i <= N ; i++) {
+                for (int j = 1 ; j <= N ; j++) {
+                    if (i == j)
+                        continue;
+                    if (map[i][k] != 0 && map[k][j] != 0) {
+                        if (map[i][j] == 0) {
+                            map[i][j] = map[i][k] + map[k][j];
+                        } else {
+                            map[i][j] = Math.min(map[i][j], map[i][k] + map[k][j]);
+                        }
+                    }
                 }
             }
         }
 
         StringBuilder sb = new StringBuilder();
-        for (int i = 1 ; i <= n ; i++) {
-            for (int j = 1 ; j <= n ; j++) {
-                if (dist[i][j] == INF) {
-                    sb.append(0).append(" ");
-                } else
-                    sb.append(dist[i][j]).append(" ");
+
+        for (int i = 1 ; i <= N ; i++) {
+            for (int j = 1 ; j <= N ; j++) {
+                sb.append(map[i][j]).append(" ");
             }
             sb.append("\n");
         }
 
-        System.out.print(sb);
+        System.out.println(sb);
     }
 }
