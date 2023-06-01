1fa56d2d04c3a599691082eea156f1c4dc0d8f59
Sangmin Ju <besforyou2@gmail.com>
2023-06-01 23:40:32 +0900

    [Silver II] Title: 병사 배치하기, Time: 120 ms, Memory: 12016 KB -BaekjoonHub

---
+++ 백준/Silver/18353. 병사 배치하기/병사 배치하기.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int [] arr = new int[N + 1];
+        int [] dp = new int[N + 1];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int n = 1 ; n <= N ; n++) {
+            arr[N + 1 - n] = Integer.parseInt(st.nextToken());
+            dp[n] = 1;
+        }
+
+        for (int i = 1 ; i <= N ; i++) {
+            for (int j = 1 ; j <= i ; j++) {
+                if (arr[j] < arr[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        int max = -1;
+
+        for (int i = 1 ; i <= N ; i++) {
+            max = Math.max(max, dp[i]);
+        }
+
+        System.out.println(N - max);
+
+    }
+}
