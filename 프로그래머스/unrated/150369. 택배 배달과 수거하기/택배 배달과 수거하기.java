19221f645859f9b69f8baae466df2960448dd2f8
Sangmin Ju <besforyou2@gmail.com>
2023-06-08 15:34:02 +0900

    [unrated] Title: 택배 배달과 수거하기, Time: 226.82 ms, Memory: 184 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+class Solution {
+    public long solution(int cap, int n, int[] deliveries, int[] pickups) {
+        long answer = 0;
+
+        Stack<Integer> delivery = new Stack<>();
+        Stack<Integer> pickup = new Stack<>();
+
+        // { 배달/수거, 인덱스 } object를 stack에 담기
+        for (int i = 0 ; i < n ; i++) {
+            for (int j = 0 ; j < deliveries[i] ; j++) {
+                delivery.push(i + 1);
+            }
+            for (int j = 0 ; j < pickups[i] ; j++) {
+                pickup.push(i + 1);
+            }
+        }
+
+        while (!delivery.isEmpty() && !pickup.isEmpty()) {
+            int lastD = delivery.peek();
+            int lastP = pickup.peek();
+
+            for (int i = 0 ; i < cap ; i++) {
+                if(!delivery.isEmpty()) delivery.pop();
+                if(!pickup.isEmpty()) pickup.pop();
+            }
+
+            answer += Math.max(lastD, lastP) * 2L;
+        }
+
+        while(!delivery.isEmpty()) {
+            int last = delivery.peek();
+
+            for (int i = 0 ; i < cap ; i++)
+                if(!delivery.isEmpty()) delivery.pop();
+
+            answer += last * 2L;
+        }
+
+        while(!pickup.isEmpty()) {
+            int last = pickup.peek();
+
+            for (int i = 0 ; i < cap ; i++)
+                if(!pickup.isEmpty()) pickup.pop();
+
+            answer += last * 2L;
+        }
+
+        return answer;
+    }
+}
