6d3fc1a46d854875a05c69c9ee77decd34ed9e12
Sangmin Ju <besforyou2@gmail.com>
2023-07-17 13:46:08 +0900

    [Gold II] Title: 반도체 설계, Time: 212 ms, Memory: 19876 KB -BaekjoonHub

--- 백준/Gold/2352. 반도체 설계/반도체 설계.java
+++ 백준/Gold/2352. 반도체 설계/반도체 설계.java
@@ -2,43 +2,45 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 
     static int N;
-    static int [] arr, dp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         N = Integer.parseInt(br.readLine());
 
-        arr = new int[N + 1];
-        dp = new int[N + 1];
+        ArrayList<Integer> seq = new ArrayList<>();
+        seq.add(-2_000_000_000);
 
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         for (int n = 1 ; n < N + 1 ; n++ ) {
-            arr[n] = Integer.parseInt(st.nextToken());
-            dp[n] = 1;
-        }
+            int val = Integer.parseInt(st.nextToken());
+            int len = seq.size();
 
-        for (int i = 1 ; i < N + 1 ; i++) {
-            for (int j = 1 ; j <= i ; j++) {
-                if (arr[i] > arr[j]) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
+            if (seq.get(len - 1) < val) {
+                seq.add(val);
+            } else {
+                int lp = 0;
+                int rp = len - 1;
+
+                while (lp + 1 < rp) {
+                    int mid = (lp + rp) / 2;
+
+                    if (seq.get(mid) < val) {
+                        lp = mid;
+                    } else {
+                        rp = mid;
+                    }
                 }
+                seq.set(rp, val);
             }
         }
 
-        int max = -1;
-
-        for (int a : dp) {
-             max = Math.max(max, a);
-        }
-
-        System.out.println(max);
+        System.out.println(seq.size() - 1);
     }
 }
