4802e483aabd6d2d9a4276dca58ec22c95cee4a9
Sangmin Ju <besforyou2@gmail.com>
2023-07-24 15:42:58 +0900

    [Gold IV] Title: 해킹, Time: 964 ms, Memory: 153192 KB -BaekjoonHub

--- 백준/Gold/10282. 해킹/해킹.java
+++ 백준/Gold/10282. 해킹/해킹.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
@@ -10,21 +9,24 @@ public class Main {
 
     static class Edge implements Comparable<Edge>{
         int node, weight;
-        Edge(int n, int w) {
+
+        Edge (int n, int w) {
             this.node = n;
             this.weight = w;
         }
 
         @Override
-        public int compareTo(Edge o) {
-            return Integer.compare(this.weight, o.weight);
+        public int compareTo(Edge e) {
+            return Integer.compare(this.weight, e.weight);
         }
     }
 
-    static int n, d, c;
-    static final int INF = 1_000_000_000;
-    static ArrayList<Edge>[] edges;
-    static int [] distance;
+    static int INF = 1_000_000_000;
+    static ArrayList<Edge> [] edges;
+    static int [] dist;
+
+    static int N, D, C;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -32,22 +34,22 @@ public class Main {
 
         int T = Integer.parseInt(br.readLine());
 
-
         for (int t = 0 ; t < T ; t++) {
+
             StringTokenizer st = new StringTokenizer(br.readLine());
-            n = Integer.parseInt(st.nextToken());
-            d = Integer.parseInt(st.nextToken());
-            c = Integer.parseInt(st.nextToken());
+            N = Integer.parseInt(st.nextToken());
+            D = Integer.parseInt(st.nextToken());
+            C = Integer.parseInt(st.nextToken());
 
-            edges = new ArrayList[n + 1];
-            distance = new int[n + 1];
+            edges = new ArrayList[N + 1];
+            dist = new int[N + 1];
 
-            for (int i = 0 ; i < n + 1 ; i++) {
+            for (int i = 0 ; i < N + 1 ; i++) {
                 edges[i] = new ArrayList<>();
-                distance[i] = INF;
+                dist[i] = INF;
             }
 
-            for (int i = 0 ; i < d ; i++) {
+            for (int i = 0 ; i < D ; i++) {
                 st = new StringTokenizer(br.readLine());
                 int a = Integer.parseInt(st.nextToken());
                 int b = Integer.parseInt(st.nextToken());
@@ -55,34 +57,14 @@ public class Main {
                 edges[b].add(new Edge(a, s));
             }
 
-            PriorityQueue<Edge> pq = new PriorityQueue<>();
-            pq.add(new Edge(c, 0));
-            distance[c] = 0;
-
-            boolean [] visit = new boolean[n + 1];
-
-            while (!pq.isEmpty()) {
-
-                Edge edge = pq.poll();
-
-                if (!visit[edge.node]) {
-                    visit[edge.node] = true;
-
-                    for (Edge next : edges[edge.node]) {
-                        if (distance[next.node] > distance[edge.node] + next.weight) {
-                            distance[next.node] = distance[edge.node] + next.weight;
-                            pq.add(new Edge(next.node, distance[next.node]));
-                        }
-                    }
-                }
-            }
+            dijkstra(C);
 
             int cnt = 0, result = 0;
 
-            for (int i = 1 ; i < n + 1; i++) {
-                if (distance[i] != INF) {
+            for (int i = 1 ; i < N + 1 ; i++) {
+                if (dist[i] != INF) {
                     cnt += 1;
-                    result = Math.max(result, distance[i]);
+                    result = Math.max(result, dist[i]);
                 }
             }
 
@@ -90,5 +72,28 @@ public class Main {
         }
 
         System.out.print(sb);
+    }
+
+    public static void dijkstra(int start) {
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+        pq.add(new Edge(start, 0));
+        dist[start] = 0;
+
+        boolean [] visit = new boolean[N + 1];
+
+        while (!pq.isEmpty()) {
+            Edge edge = pq.poll();
+
+            if (!visit[edge.node]) {
+                visit[edge.node] = true;
+
+                for (Edge next : edges[edge.node]) {
+                    if (dist[next.node] > dist[edge.node] + next.weight) {
+                        dist[next.node] = dist[edge.node] + next.weight;
+                        pq.add(new Edge(next.node, dist[next.node]));
+                    }
+                }
+            }
+        }
     }
 }
