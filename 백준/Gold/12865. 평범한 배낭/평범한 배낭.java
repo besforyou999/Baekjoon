77b10c61562a3d6cfc643f34d1142d50f23f8734
Sangmin Ju <besforyou2@gmail.com>
2023-08-03 13:57:55 +0900

    [Gold V] Title: 평범한 배낭, Time: 140 ms, Memory: 51244 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,46 +1,42 @@
 import java.io.BufferedReader;
+import java.io.IOError;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.PriorityQueue;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringBuilder sb = new StringBuilder();
-	static StringTokenizer st;
+    static int N, K;
+    static int weight[];
+    static int value[];
 
-	static int n, m;
-	static int dp[][];
-	static int items[][];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-	public static void main(String[] args) throws IOException {
-		input();
-		for (int i = 1; i <= n; i++) {
-			for (int j = 1; j <= m; j++) {
-				dp[i][j] = dp[i - 1][j];
-				if (j - items[i][1] >= 0) {
-					dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - items[i][1]] + items[i][2]);
-				}
-			}
-		}
-		System.out.println(dp[n][m]);
-	}
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
 
-	public static void input() throws IOException {
-		st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		dp = new int[n + 1][m + 1];
-		items = new int[n + 1][2 + 1];
-		for (int i = 1; i <= n; i++) {
-			st = new StringTokenizer(br.readLine());
-			int w = Integer.parseInt(st.nextToken());
-			int v = Integer.parseInt(st.nextToken());
-			items[i][1] = w;
-			items[i][2] = v;
-		}
-	}
+        weight = new int[N + 1];
+        value = new int[N + 1];
+
+        for (int i = 1 ; i <= N ; i++) {
+            st = new StringTokenizer(br.readLine());
+            weight[i] = Integer.parseInt(st.nextToken());
+            value[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int dp [][] = new int[N + 1][K + 1];
+
+        for (int i = 1 ; i <= N ; i++) {
+            for (int j = 1 ; j <= K ; j++) {
+                if (j < weight[i]) {
+                    dp[i][j] = dp[i-1][j];
+                } else {
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j - weight[i]] + value[i]);
+                }
+            }
+        }
+
+        System.out.print(dp[N][K]);
+    }
 }
