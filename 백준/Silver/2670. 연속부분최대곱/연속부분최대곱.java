d73f565640337c69949bdfe655fb91f0dafbf286
Sangmin Ju <besforyou2@gmail.com>
2023-08-04 15:55:13 +0900

    [Silver IV] Title: 연속부분최대곱, Time: 124 ms, Memory: 14772 KB -BaekjoonHub

---
+++ 백준/Silver/2670. 연속부분최대곱/연속부분최대곱.java
@@ -0,0 +1,21 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        double[] arr = new double[n];
+        for(int i = 0; i < n; i++) {
+            arr[i] = Double.parseDouble(br.readLine());
+        }
+
+        double max = arr[0];
+
+        for(int i = 1 ; i < n; i++) {
+            arr[i] = Math.max(arr[i], arr[i-1] * arr[i]);
+            max = Math.max(max, arr[i]);
+        }
+        // 5. max를 출력한다.
+        System.out.printf("%.3f", max);
+    }
+}
