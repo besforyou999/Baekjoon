020496bb1bef45099624ba903427a2b248169df5
Sangmin Ju <besforyou2@gmail.com>
2023-08-08 11:23:45 +0900

    [Silver IV] Title: 수 찾기, Time: 552 ms, Memory: 57028 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,55 +1,46 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
 
-    static int N, M;
-    static StringTokenizer st;
-
-    static long A[];
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
 
-        N = Integer.parseInt(br.readLine());
+        int [] arr = new int[N];
 
-        A = new long[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int n = 0 ; n < N ; n++) {
+            arr[n] = Integer.parseInt(st.nextToken());
+        }
 
-        st = new StringTokenizer(br.readLine());
+        Arrays.sort(arr);
 
-        for (int n = 0 ; n < N ; n++)
-            A[n] = Long.parseLong(st.nextToken());
-
-        Arrays.sort(A);
-
-        M = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
 
         st = new StringTokenizer(br.readLine());
 
         StringBuilder sb = new StringBuilder();
 
         for (int m = 0 ; m < M ; m++) {
-            int target = Integer.parseInt(st.nextToken());
-            sb.append(bsearch(target)).append("\n");
+            sb.append(bSearch(arr, Integer.parseInt(st.nextToken()))).append("\n");
         }
 
         System.out.print(sb);
     }
 
-    public static int bsearch(int target) {
-        int lp = 0, rp = N - 1;
+    public static int bSearch(int [] arr, int target) {
+        int lp = 0, rp = arr.length - 1;
 
-        while (lp <= rp) {
+        while ( lp <= rp ) {
             int mid = (lp + rp) / 2;
-
-            if (A[mid] > target) {
-                rp = mid - 1;
-            } else if (A[mid] < target) {
-                lp = mid + 1;
-            } else
+            if (arr[mid] == target) {
                 return 1;
+            } else if (arr[mid] > target) {
+                rp = mid - 1;
+            } else {
+                lp = mid + 1;
+            }
         }
 
         return 0;
