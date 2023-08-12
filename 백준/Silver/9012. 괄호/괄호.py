852780652f2e595fe5f5b915cd903e197175e3ec
Sangmin Ju <besforyou2@gmail.com>
2023-08-12 23:53:09 +0900

    [Silver IV] Title: 괄호, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -0,0 +1,29 @@
+from sys import stdin
+
+T = int(stdin.readline())
+
+answer = ""
+
+for _ in range(T):
+    stack = []
+    line = stdin.readline().split()[0]
+    
+    for ch in line:
+        if ch == "(":
+            stack.append(ch)
+        elif ch == ")":
+            if len(stack) == 0:
+                stack.append(ch)
+            else:
+                peek = stack[-1]
+                if peek == ")":
+                    stack.append(ch)
+                elif peek == "(":
+                    stack.pop()
+
+    if len(stack) == 0:
+        answer += "YES\n"
+    else:
+        answer += "NO\n"
+
+print(answer)
