58a2016909d4aeeff0f308a3824c2ea90cc0aa75
Sangmin Ju <besforyou2@gmail.com>
2023-08-15 12:39:53 +0900

    [Gold V] Title: LCS, Time: 156 ms, Memory: 123052 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -9,17 +9,14 @@ len2 = len(line2)
 
 graph = [[0 for _ in range(len2 + 1)] for _ in range(len1 + 1)]
 
+ans = 0
+
 for i in range(1, len1 + 1):
     for j in range(1, len2 + 1):
-        if line1[i - 1] == line2[j - 1]:
+        if line2[j-1] == line1[i-1]:
             graph[i][j] = graph[i-1][j-1] + 1
         else:
             graph[i][j] = max(graph[i-1][j], graph[i][j-1])
+        ans = max(ans, graph[i][j])
 
-answer = -1
-
-for i in range(1, len1 + 1):
-    for j in range(1, len2 + 1):
-        answer = max(answer, graph[i][j])
-
-print(answer)
+print(ans)
