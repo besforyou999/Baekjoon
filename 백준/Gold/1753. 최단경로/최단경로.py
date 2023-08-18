105c015e7a607dde1792b7fb0c3262d3530ab421
Sangmin Ju <besforyou2@gmail.com>
2023-08-18 11:58:35 +0900

    [Gold IV] Title: 최단경로, Time: 652 ms, Memory: 68472 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -1,38 +1,42 @@
-import sys, heapq
-
+import sys
+import heapq
 input = sys.stdin.readline
-
 INF = sys.maxsize
+
 V, E = map(int, input().split())
 K = int(input())
 
-dp = [INF] * (V + 1)
-heap = []
 graph = [[] for _ in range(V + 1)]
-
-def Dijkstra(start):
-    dp[start] = 0
-    heapq.heappush(heap, (0, start))
-
-    while heap:
-        wei, now = heapq.heappop(heap)
-
-        if dp[now] < wei:
-            continue
-
-        for w, next_node in graph[now]:
-
-            next_wei = w + wei
-
-            if next_wei < dp[next_node]:
-                dp[next_node] = next_wei
-                heapq.heappush(heap, (next_wei, next_node))
 
 for _ in range(E):
     u, v, w = map(int, input().split())
     graph[u].append((w, v))
 
-Dijkstra(K)
+heap = []
+dp = [INF] * (V + 1)
 
+
+def Dijstra(start):
+
+    heapq.heappush(heap, (0, start))
+
+    while heap:
+        w, node = heapq.heappop(heap)
+
+        if dp[node] < w:
+            continue
+
+        for weight, next_node in graph[node]:
+            next_weight = w + weight
+            if dp[next_node] > next_weight:
+                dp[next_node] = next_weight
+                heapq.heappush(heap, (next_weight, next_node))
+
+
+dp[K] = 0
+Dijstra(K)
 for i in range(1, V + 1):
-    print("INF" if dp[i] == INF else dp[i])
+    if dp[i] == INF:
+        print("INF")
+    else:
+        print(dp[i])
