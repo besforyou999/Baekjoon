2d5b8b5d9378df530087009fc81cd47957b53ad5
Sangmin Ju <besforyou2@gmail.com>
2023-08-25 11:41:27 +0900

    [Silver III] Title: N과 M (1), Time: 172 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,23 +1,17 @@
 N, M = map(int, input().split())
 
+arr = []
 
-def perm(depth, visit, rec, arr):
-    if depth == M:
-        for i in rec:
-            print(i, end=' ')
-        print()
+
+def dfs():
+    if len(arr) == M:
+        print(' '.join(map(str, arr)))
         return
 
-    for i in range(len(arr)):
-        if not visit[i]:
-            visit[i] = True
-            rec[depth] = arr[i]
-            perm(depth + 1, visit, rec, arr)
-            visit[i] = False
+    for i in range(1, N + 1):
+        if i not in arr:
+            arr.append(i)
+            dfs()
+            arr.pop()
 
-
-arr = [i for i in range(1, N + 1)]
-rec = [0] * M
-visit = [False] * N
-
-perm(0, visit, rec, arr)
+dfs()
