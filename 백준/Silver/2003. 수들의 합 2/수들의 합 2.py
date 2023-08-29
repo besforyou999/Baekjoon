727e34eb796ad417d988ad204395412de48e752d
Sangmin Ju <besforyou2@gmail.com>
2023-08-29 13:26:50 +0900

    [Silver IV] Title: 수들의 합 2, Time: 508 ms, Memory: 32276 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.py
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.py
@@ -1,30 +1,19 @@
-import sys
-input = sys.stdin.readline
-
 N, M = map(int, input().split())
-Ai = list(map(int, input().split()))
+arr = list(map(int, input().split()))
 
-if N == 1:
-    if Ai[0] == M:
-        print(1)
+left, right = 0, 1
+cnt = 0
+
+while right <= N and left <= N:
+
+    nums = arr[left:right]
+    tmp = sum(nums)
+    if tmp < M:
+        right += 1
+    elif tmp > M:
+        left += 1
     else:
-        print(0)
-    sys.exit(0)
+        right += 1
+        cnt += 1
 
-lp = 0
-rp = lp + 1
-count = 0
-while lp < N:
-    tmp = Ai[lp]
-    while rp < N:
-        if tmp < M:
-            tmp += Ai[rp]
-            rp += 1
-        elif tmp >= M:
-            break
-    if tmp == M:
-        count += 1
-    lp += 1
-    rp = lp + 1
-
-print(count)
+print(cnt)
