e10712d595180c9b209b4d1d2546f8d453a5ca3c
Sangmin Ju <besforyou2@gmail.com>
2023-09-01 16:13:02 +0900

    [Gold V] Title: 경쟁적 전염, Time: 204 ms, Memory: 116452 KB -BaekjoonHub

--- 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
+++ 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
@@ -1,5 +1,3 @@
-from collections import deque
-
 N, K = map(int, input().split())
 
 dr = [-1, 1, 0, 0]
@@ -14,27 +12,25 @@ graph = []
 for _ in range(N):
     graph.append(list(map(int, input().split())))
 
-virus_arr = []
+virus_arr = [[] for _ in range(K + 1)]
 
 for r in range(N):
     for c in range(N):
         if graph[r][c] > 0:
-            virus_arr.append((graph[r][c], r, c))
-
-virus_arr.sort(key=lambda e: e[0])
-queue = deque(virus_arr)
+            virus_arr[graph[r][c]].append((r, c))
 
 S, X, Y = map(int, input().split())
 
 for _ in range(S):
-    size = len(queue)
-    for _ in range(size):
-        virus, r, c = queue.popleft()
-        for i in range(4):
-            nr = r + dr[i]
-            nc = c + dc[i]
-            if is_in(nr, nc) and graph[nr][nc] == 0:
-                graph[nr][nc] = virus
-                queue.append((virus, nr, nc))
+    for i in range(1, K + 1):
+        new_arr = []
+        for r, c in virus_arr[i]:
+            for d in range(4):
+                nr = r + dr[d]
+                nc = c + dc[d]
+                if is_in(nr, nc) and graph[nr][nc] == 0:
+                    graph[nr][nc] = i
+                    new_arr.append((nr, nc))
+        virus_arr[i] = new_arr
 
 print(graph[X - 1][Y - 1])
