365c3ec0fa652d21997483e3e102e04f8fdf3eea
Sangmin Ju <besforyou2@gmail.com>
2023-09-02 17:54:25 +0900

    [Gold V] Title: 마법사 상어와 비바라기, Time: 512 ms, Memory: 117268 KB -BaekjoonHub

---
+++ 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.py
@@ -0,0 +1,48 @@
+# 입력
+N, M = map(int, input().split())
+board = [list(map(int, input().split())) for _ in range(N)]
+moves = [tuple(map(int, input().split())) for _ in range(M)]
+
+# 8방향
+dy8 = ("empty", 0, -1, -1, -1, 0, 1, 1, 1)
+dx8 = ("empty", -1, -1, 0, 1, 1, 1, 0, -1)
+
+# 대각 4방향
+dy4 = (-1, -1, 1, 1)
+dx4 = (-1, 1, -1, 1)
+
+clouds = [(N - 1, 0), (N - 1, 1), (N - 2, 0), (N - 2, 1)]
+for d, s in moves:
+    # 모든 구름 이동
+    moved_clouds = []
+    for y, x in clouds:
+        ny = (y + dy8[d] * s) % N
+        nx = (x + dx8[d] * s) % N
+        board[ny][nx] += 1  # 물의 양 추가
+        moved_clouds.append((ny, nx))  # 이동한 구름 좌표에 추가
+
+    for y, x in moved_clouds:
+        cnt = 0
+        for d in range(4):
+            ny = y + dy4[d]
+            nx = x + dx4[d]
+            if ny < 0 or nx < 0 or ny >= N or nx >= N:
+                continue
+            elif board[ny][nx] > 0:
+                cnt += 1
+        board[y][x] += cnt
+
+    new_clouds = []
+    for y in range(N):
+        for x in range(N):
+            if (y, x) in moved_clouds or board[y][x] < 2:
+                continue
+            board[y][x] -= 2
+            new_clouds.append((y, x))
+    clouds = new_clouds
+
+result = 0
+for y in range(N):
+    for x in range(N):
+        result += board[y][x]
+print(result)
