5c7393a45d4c7cfb156e3f81e0ce9ab6f094cbfa
Sangmin Ju <besforyou2@gmail.com>
2023-10-19 20:16:30 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -1,26 +1,3 @@
-def check_valid(s):
-    one, two, three = 0, 0, 0
-
-    for ch in s:
-        if ch == '[':
-            one += 1
-        elif ch == ']':
-            one -= 1
-        elif ch == '(':
-            two += 1
-        elif ch == ')':
-            two -= 1
-        elif ch == '{':
-            three += 1
-        else:
-            three -= 1
-
-    if (one, two, three) == (0, 0, 0):
-        return True
-
-    return False
-
-
 def check_correct(s):
     stack = []
 
@@ -47,25 +24,17 @@ def check_correct(s):
             else:
                 stack.pop()
 
-    return True
-
-
-def left_rotate(s):
-    first = s[0]
-    body = s[1:]
-    return body + first
+    
+    return False if stack else True
 
 
 def solution(s):
-    if not check_valid(s):
-        return 0
-
+    
     answer = 0
     size = len(s)
 
     for x in range(size):
-        if check_correct(s):
+        if check_correct(s[x:] + s[:x]):
             answer += 1
-        s = left_rotate(s)
 
     return answer
