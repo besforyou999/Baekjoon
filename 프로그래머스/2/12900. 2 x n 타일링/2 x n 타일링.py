c2dc3e61e0be406a23493b933946e039a963dd7b
Sangmin Ju <besforyou2@gmail.com>
2023-10-22 17:50:55 +0900

    [level 2] Title: 2 x n 타일링, Time: 12.36 ms, Memory: 11.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.py
@@ -0,0 +1,15 @@
+def solution(n):
+    
+    if n <= 3:
+        return n
+    
+    DIV = 1000000007
+    
+    dp = [0] * (n + 1)
+    dp[1] = 1
+    dp[2] = 2
+    
+    for i in range(3, n + 1):
+        dp[i] = (dp[i-1] + dp[i-2]) % DIV
+    
+    return dp[n]
